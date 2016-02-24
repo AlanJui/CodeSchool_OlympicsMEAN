6e360684ddc975fa4b7f10ade9da78be439c6015
Alan Jui <alanjui.1960@gmail.com>
2016-02-24 23:06:02 +0800

    Client端改用 API ，取得 Sport Items

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,10 @@
 import angular from 'angular';
 
 angular.module('olympics', [])
-  .controller('sportsController', function () {
-    this.sports = ['Weightlifting', 'Cycling'];
-  });
+  .controller('sportsController', ['$http', function ($http) {
+
+    $http.get('/sports')
+      .then( (response) => {
+        this.sports = response.data;
+      });
+  }]);
