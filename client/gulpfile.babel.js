629f6b7ce3d15504251e244d16a001bbac2774fc
Alan Jui <alanjui.1960@gmail.com>
2016-02-25 10:37:44 +0800

    變更 gulp watch ，使debug功能可於Chrome DevTools運作

--- client/gulpfile.babel.js
+++ client/gulpfile.babel.js
@@ -1,20 +1,42 @@
 import gulp from "gulp";
 import browserify from "browserify";
 import source from "vinyl-source-stream";
+import fs from "fs";
 
 gulp.task("default", ["transpile"]);
 
 gulp.task("transpile", () => {
 
-  return browserify("src/app.js")
-    .transform("babelify")
-    .bundle()
-    .on("error", function(error){
-      console.error( "\nError: ", error.message, "\n");
-      this.emit("end");
-    })
-    .pipe(source("bundle.js"))
-    .pipe(gulp.dest("dist"));
+  return browserify({debug: true})
+            .transform('babelify')
+            .require('src/app.js', {entry: true})
+            .bundle()
+            .on('error', function (error) {
+              console.error(`\nError: ${error.message}\n`);
+              this.emit('end');
+            })
+            .pipe(fs.createWriteStream('dist/bundle.js'));
+
+  // return browserify({debug: true})
+  //           .transform('babelify')
+  //           .require('src/app.js', {entry: true})
+  //           .bundle()
+  //           .on('error', function (error) {
+  //             console.error(`\nError: ${error.message}\n`);
+  //             this.emit('end');
+  //           })
+  //           .pipe(source('bundle.js'))
+  //           .pipe(gulp.dest('dist'));
+
+  // return browserify("src/app.js")
+  //   .transform("babelify")
+  //   .bundle()
+  //   .on("error", function(error){
+  //     console.error( "\nError: ", error.message, "\n");
+  //     this.emit("end");
+  //   })
+  //   .pipe(source("bundle.js"))
+  //   .pipe(gulp.dest("dist"));
 
 });
 
@@ -23,4 +45,3 @@ gulp.task("transpile", () => {
 gulp.task("watch", ["transpile"], () => {
   gulp.watch("src/**/*", ["transpile"]);
 });
-
