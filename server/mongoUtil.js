4cd9ec6f4b1bbac9e48ec864f70353e1b3c13d28
Alan Jui <alanjui.1960@gmail.com>
2016-02-25 12:19:34 +0800

    在Server端加入MongoDB伺服器；同時變更 API: /sports ，使「資料來源」由MongoDB伺服器提供

--- server/mongoUtil.js
+++ server/mongoUtil.js
@@ -1,21 +1,23 @@
-"use strict";
+'use strict';
 
 let mongo = require('mongodb');
 let client = mongo.MongoClient;
 let _db;
 
 module.exports = {
+
   connect() {
     client.connect('mongodb://localhost:27017/olympics-dev', (err, db) => {
-      if(err) {
-        console.log("Error connecting to Mongo - check mongod connection");
+      if (err) {
+        console.error("Error connecting to MongoDB server - check mongod has been started");
         process.exit(1);
       }
       _db = db;
-      console.log("Connected to Mongo");
-    });
+      console.log('MongoDB Server connected');
+    })
   },
-  sports(){
+
+  sports() {
     return _db.collection('sports');
   }
-}
+};
